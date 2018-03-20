9db2a0403ceb3fa3f5cf5a27a242012ddab2abc4
shettyempire <shetty_sachins@yahoo.in>
2018-03-21 00:47:19 +0530

    new functions added

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("added index js file");
-console.log("added grades");
+var grades = function(){
+    console.log("Grades of student");
+};
+
+var studentData = function(){
+    console.log("student info");
+}
