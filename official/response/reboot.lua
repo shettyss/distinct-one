064bcb33f01e0fa4cdb6d0939d56df7e86b24dcc
shettyss <shettyss27@gmail.com>
2022-03-14 12:08:33 +0530

    Add new files

---
+++ official/response/reboot.lua
@@ -0,0 +1,28 @@
+--[=[
+name: Force System Reboot O4
+filetype: Infocyte Extension
+type: Response
+description: Forces system reboot after delay
+author: Infocyte
+guid: nm031ce0-75c4-42d9-a2b3-d32fad3b69iw
+created: 2022-01-22
+updated: 2022-12-14
+# Global variables
+globals:
+- reboot_reason:
+    description: Default reason message to display to user and input in logs
+    type: string
+    default: Infocyte
+# Runtime arguments
+args:
+- reason:
+    description: Reason message to display to user and input in logs
+    type: string
+    required: false
+]=]
+
+--[=[ SECTION 1: Inputs ]=]
+-- hunt.arg(name = <string>, isRequired = <boolean>, [default])
+-- hunt.global(name = <string>, isRequired = <boolean>, [default])
+
+default_reason = "Infocyte"
